b87b6f78c3b4df0200c3a7f860b391decde0e216
Sirjay009 <obuttechima@rocketmail.com>
2025-06-17 00:36:30 +0000

    Modify script using the correct input shape

--- convert_to_tflite.py
+++ convert_to_tflite.py
@@ -4,11 +4,13 @@ import tensorflow as tf
 keras_model_path = 'outputs/v1/mildew_detector_model.keras'
 model = tf.keras.models.load_model(keras_model_path)
 
-# Convert to TensorFlow Lite
+# Build the model with the input shape
+model.build(input_shape=(None, 256, 256, 3))
+
+# Convert the model to TFLite
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-# Save the converted model
-tflite_model_path = 'outputs/v1/mildew_detector_model.tflite'
-with open(tflite_model_path, 'wb') as f:
+# Save the TFLite model
+with open('outputs/v1/mildew_detector_model.tflite', 'wb') as f:
     f.write(tflite_model)
